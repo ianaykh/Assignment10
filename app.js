eeeaf8d19831495635996cb0096ae00ddbb9afee
Anay Uday Khairatkar <anaykhairatkar@gmail.com>
2015-05-04 15:46:56 -0700

    Added cool angular js

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
-var main = function () {
-    "use strict";
+var app = angular.module('myapp', []); // module created "myapp"
 
-    var addCommentFromInputBox = function () {
-        var $new_comment;
-
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("");
+app.controller("myappctrl", function($scope) { // The controller name "myappcntrl"
+    $scope.message = 'angularjs is cool';
+    $scope.list = ['comment1', 'comment2', 'comment3'];
+    $scope.addcomment = function() {
+        if ($scope.appmodel == null) {
+            alert("Pls add some text");
+        } else {
+            $scope.list.push($scope.appmodel);
+            $scope.message1 = 'Comment is added';
         }
     };
 
-    $(".comment-input button").on("click", function (event) {
-        addCommentFromInputBox();
-    });
 
-    $(".comment-input input").on("keypress", function (event) {
-        if (event.keyCode === 13) {
-            addCommentFromInputBox();
-        }
-    });
-};
+});
 
-$(document).ready(main);
+
+
+
+
+
+
+
+
+
+// var main = function() {
+//     "use strict";
+
+//     var addCommentFromInputBox = function() {
+//         var $new_comment;
+
+//         if ($(".comment-input input").val() !== "") {
+//             $new_comment = $("<p>").text($(".comment-input input").val());
+//             $new_comment.hide();
+//             $(".comments").append($new_comment);
+//             $new_comment.fadeIn();
+//             $(".comment-input input").val("");
+//         }
+//     };
+
+//     $(".comment-input button").on("click", function(event) {
+//         addCommentFromInputBox();
+//     });
+
+//     $(".comment-input input").on("keypress", function(event) {
+//         if (event.keyCode === 13) {
+//             addCommentFromInputBox();
+//         }
+//     });
+// };
+
+
+// $(document).ready(main);
